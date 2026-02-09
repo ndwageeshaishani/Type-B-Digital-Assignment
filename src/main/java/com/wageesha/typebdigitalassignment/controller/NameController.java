219a7e5cdb155e85dddfe0a623fb2ab16ab7ef9c
ndwageeshaishani <ndwageeshaishani@gmail.com>
2026-02-09 14:43:49 +0530

    @RequestMapping removed.

--- src/main/java/com/wageesha/typebdigitalassignment/controller/NameController.java
+++ src/main/java/com/wageesha/typebdigitalassignment/controller/NameController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +13,6 @@ import static com.wageesha.typebdigitalassignment.common.ConstantData.PATH_HELLO
 
 @Slf4j
 @RestController
-@RequestMapping
 @RequiredArgsConstructor
 public class NameController {
 
